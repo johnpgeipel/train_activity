d6b4c53e72af9635f82a3b706b4fd47ab3713f2b
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-05-16 15:08:55 -0500

    fixed the issues regarding the moment.js. the math wasn't correct, so the arrival time was always off by a few minutes.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,33 +13,46 @@ var firebaseConfig = {
 
   var database = firebase.database();
 
+    //   declare variables for the onClick event
+    var trainName;
+    var destinationName;
+    var firstTrain;
+    var freqTrain = 0;
+
+
+  
+
   $("#add-train-btn").on("click", function(event) {
       event.preventDefault();
 
     //   train input from forms
-    var trainName = $("#train-name-input").val().trim();
-    var destinationName = $("#destination-name-input").val().trim();
-    var firstTrain = moment($("#first-train-input").val().trim(), "HH:mm").format("X");
-    var freqTrain = $("#frequency-input").val().trim();
+    trainName = $("#train-name-input").val().trim();
+    destinationName = $("#destination-name-input").val().trim();
+    firstTrain = $("#first-train-input").val().trim();
+    freqTrain = $("#frequency-input").val().trim();
 
     // create variable for holding new train info
-    var newTrain = {
-        name: trainName,
-        destination: destinationName,
-        first: firstTrain,
-        frequency: freqTrain
+  var newTrain = {
+    name: trainName,
+    destination: destinationName,
+    firstTrain: firstTrain,
+    frequency: freqTrain
+    
     };
+    
+
+    
 
     // adds newTrain info to the firebase database
     database.ref().push(newTrain);
 
     console.log(newTrain.name)
     console.log(newTrain.destination)
-    console.log(newTrain.first)
+    console.log(newTrain.firstTrain)
     console.log(newTrain.frequency)
 
     // Alerts user to new train being added to roster
-    alert("New train added!");
+    // alert("New train added!");
 
     // Clears the input fields after submission of newTrain info
     $("#train-name-input").val("");
@@ -50,53 +63,47 @@ var firebaseConfig = {
 
 database.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());
+    // declare variables for next arrival and minutes away
+    var nextArr;
+    var minAway;
 
-    // Stores the data into variables to prepend the database
+    var currentTime = moment().format("hh:mm");
+    console.log(currentTime)
+
+    // set time back one year from snapshot value
+    var firstTrainNew = moment(childSnapshot.val().firstTrain,"hh:mm").subtract(1, "year");
+
+    // calculate differance between current and firstTrain
+    var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+    console.log("Diff: " + diffTime);
+
+    // timeRemainder calculates the modulus of the diffence and the snapshot of the frequency
+    var timeRemainder = diffTime % childSnapshot.val().frequency;
+    console.log("Remainder: " + timeRemainder)
+
+    var minAway = childSnapshot.val().frequency - timeRemainder;
+
+    console.log("Min Away: " + minAway)
+
+    var nextArr = moment().add(minAway, "minutes");
+        nextArr = moment(nextArr).format("hh:mm");
+
+
+
+
+    // Stores the data into variables to prepend the datatable
     var trainName = childSnapshot.val().name;
     var destinationName = childSnapshot.val().destination;
-    var firstTrain = childSnapshot.val().first;
+    var firstTrain = childSnapshot.val().firstTrain;
     var freqTrain = childSnapshot.val().frequency;
 
-    // use moment.js to determine the next arriving train
-    // use moment.js to determine the ETA in minutes based on the arrival time and current time
-
-   
     
-
-    // moment.js to determine difference in currentTime and first train
-
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
-    // moment.js to establish current time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % freqTrain;
-    console.log(tRemainder);
-
-    // Minute Until Train
-    var tMinutesTillTrain = freqTrain - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrainTime = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrainTime).format("hh:mm"));
-    // possibly use interval or clock to show countdown of arrival
-
-    // append results to the row in the New Train data table
     var trainRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destinationName),
         $("<td>").text(freqTrain),
-        $("<td>").text(moment(nextTrainTime).format("hh:mm")),
-        $("<td>").text(tMinutesTillTrain)
+        $("<td>").text(nextArr),
+        $("<td>").text(minAway)
     );
 
     $("#schedule-table > tbody").append(trainRow);
