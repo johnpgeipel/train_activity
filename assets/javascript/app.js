49b6a4c74de6c24e658c5ae0e904e1342e6f49cb
john geipel <47228329+johnpgeipel@users.noreply.github.com>
2021-08-23 18:17:06 -0500

    rm whitespace

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-
-
-
-
 // Firebase config script
 var firebaseConfig = {
     apiKey: "AIzaSyAP32V0fY_5UhW9yzWxz22j7ew_8XQlep4",
@@ -16,7 +12,6 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
   var database = firebase.database();
-
     //   declare variables for the onClick event
     var trainName;
     var destinationName;
@@ -24,24 +19,16 @@ var firebaseConfig = {
     var freqTrain = 0;
     var clockTime;
 
-    // var clockTime = moment().format('LTS');
-    // $(".clock").text(clockTime)
-
     function displayTime() {
         var time = moment().format('HH:mm');
         $('.clock').html(time);
         setTimeout(displayTime, 1000);
     }
 $(document).ready(function() {
-        displayTime();
-    
- 
-  
+  displayTime();
 
   $("#add-train-btn").on("click", function(event) {
       event.preventDefault();
-
-    
     //   train input from forms
     trainName = $("#train-name-input").val().trim();
     destinationName = $("#destination-name-input").val().trim();
@@ -55,22 +42,16 @@ $(document).ready(function() {
       $("#destination-name-input").val("");
       $("#first-train-input").val("");
       $("#frequency-input").val("");
-      
-      
-      
     } else {
-
     // create variable for holding new train info
       var newTrain = {
         name: trainName,
         destination: destinationName,
         firstTrain: firstTrain,
         frequency: freqTrain
-        
         };
     
       database.ref().push(newTrain);
-
    
       $("#train-name-input").val("");
       $("#destination-name-input").val("");
@@ -79,74 +60,53 @@ $(document).ready(function() {
     }
   });
     
-database.ref().on("child_added", function(childSnapshot) {
-        console.log(childSnapshot.val());
-        // declare variables for next arrival and minutes away
-        var nextArr;
-        var minAway;
-        
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+    // declare variables for next arrival and minutes away
+    var nextArr;
+    var minAway;
+    var currentTime = moment().format("hh:mm");
+    console.log(currentTime)
+    // set time back one year from snapshot value
+    var firstTrainNew = moment(childSnapshot.val().firstTrain,"hh:mm").subtract(1, "year");
+    // calculate differance between current and firstTrain
+    var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+    console.log("Diff: " + diffTime);
+    // timeRemainder calculates the modulus of the diffence and the snapshot of the frequency
+    var timeRemainder = diffTime % childSnapshot.val().frequency;
+    console.log("Remainder: " + timeRemainder)
+    var minAway = childSnapshot.val().frequency - timeRemainder;
 
-        
+    if (!minAway || isNaN(minAway)) {
+      // invalidInfo();
+      alert("The information you entered is invalid. Please try again.");
+      console.log("is NaN");
+      // break
+    } else {
+    console.log("Min Away: " + minAway)
 
-        
+    var nextArr = moment().add(minAway, "minutes");
+        nextArr = moment(nextArr).format("hh:mm");
 
-        var currentTime = moment().format("hh:mm");
-        console.log(currentTime)
-
-        // set time back one year from snapshot value
-        var firstTrainNew = moment(childSnapshot.val().firstTrain,"hh:mm").subtract(1, "year");
-
-        // calculate differance between current and firstTrain
-        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
-        console.log("Diff: " + diffTime);
-
-        // timeRemainder calculates the modulus of the diffence and the snapshot of the frequency
-        var timeRemainder = diffTime % childSnapshot.val().frequency;
-        console.log("Remainder: " + timeRemainder)
-
-        var minAway = childSnapshot.val().frequency - timeRemainder;
-        
-  
-        if (!minAway || isNaN(minAway)) {
-          // invalidInfo();
-          alert("The information you entered is invalid. Please try again.");
-          console.log("is NaN");
-          // break
-        } else {
-
-        
-
-        console.log("Min Away: " + minAway)
-
-        var nextArr = moment().add(minAway, "minutes");
-            nextArr = moment(nextArr).format("hh:mm");
-        // if (!nextArr) {
-        //     invalidInfo();
-        //       // break
-        //     }
-
-
-
-        // Stores the data into variables to prepend the datatable
-        var trainName = childSnapshot.val().name;
-        var destinationName = childSnapshot.val().destination;
-        var firstTrain = childSnapshot.val().firstTrain;
-        var freqTrain = childSnapshot.val().frequency;
-
-        
-        var trainRow = $("<tr>").append(
-            $("<td>").text(trainName),
-            $("<td>").text(destinationName),
-            $("<td>").text(freqTrain),
-            $("<td>").text(nextArr),
-            $("<td>").text(minAway)
-        );
-
-        $("#schedule-table > tbody").append(trainRow);
-        console.log(trainRow)
-
-        }
-      })
+    // Stores the data into variables to prepend the datatable
+    var trainName = childSnapshot.val().name;
+    var destinationName = childSnapshot.val().destination;
+    // var firstTrain = childSnapshot.val().firstTrain;
+    var freqTrain = childSnapshot.val().frequency;
     
-});
+    var trainRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destinationName),
+        $("<td>").text(freqTrain),
+        $("<td>").text(nextArr),
+        $("<td>").text(minAway)
+    );
 
+    $("#schedule-table > tbody").append(trainRow);
+    console.log(trainRow)
+    }
+  })
+  // dynamic update to footer
+  var date = new Date().getFullYear();
+    $("#copy-year").html("<span>&copy; John Geipel " + date + "</span>");
+});
